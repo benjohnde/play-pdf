816320b6c09bfddfff48c1950bfb7eda818ef626
Jörg Viola <joerg@joergviola.de>
2012-05-21 22:34:53 +0200

    Release 0.2

--- samples/pdf-sample/project/Build.scala
+++ samples/pdf-sample/project/Build.scala
@@ -5,14 +5,14 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "pdf-sample"
-    val appVersion      = "0.1"
+    val appVersion      = "0.2"
 
     val appDependencies = Seq(
-      "pdf" % "pdf_2.9.1" % "0.1"
+      "pdf" % "pdf_2.9.1" % "0.2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      resolvers += Resolver.url("My GitHub Play Repository", url("http://joergviola.github.com/releases/"))(Resolver.ivyStylePatterns)
+      resolvers += Resolver.url("My GitHub Play Repository", url("http://www.joergviola.de/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
