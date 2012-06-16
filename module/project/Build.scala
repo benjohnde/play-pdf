edf09963d03dadbd18dd6ef5c5923ff67b38bfe6
raisercostin <raisercostin@gmail.com>
2012-06-16 15:59:15 +0200

    * add tidy support to cleanup the rendered page as xhtml before is passed to pdf api.

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -5,11 +5,12 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "pdf"
-    val appVersion      = "0.3"
+    val appVersion      = "0.4"
 
     val appDependencies = Seq(
-      "org.xhtmlrenderer" % "core-renderer"  % "R8"
-    )
+      "org.xhtmlrenderer" % "core-renderer" % "R8",
+        "net.sf.jtidy" % "jtidy" % "r938"
+	)
 
     val main = PlayProject(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
