d4414cfcaf27bffa25948ad3e5aca56fb293ae2a
Donovan de Kuiper <donovan@rhinofly.nl>
2015-03-27 10:25:00 +0100

    Removed Play framework dependencies

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ releaseSettings
 
 ReleaseKeys.crossBuild := true
 
-name := "play-pdf"
+name := "scala-pdf"
 
 organization := "nl.rhinofly"
 
@@ -11,7 +11,6 @@ scalaVersion := "2.11.4"
 crossScalaVersions := Seq("2.10.4", scalaVersion.value)
 
 libraryDependencies ++= Seq(
-  "com.typesafe.play" %% "play" % "2.3.6" % "provided",
   "org.xhtmlrenderer" % "flying-saucer-core" % "9.0.7",
   "org.xhtmlrenderer" % "flying-saucer-pdf" % "9.0.7",
   "net.sf.jtidy" % "jtidy" % "r938"
@@ -20,10 +19,6 @@ libraryDependencies ++= Seq(
 resolvers ++= Seq(
   "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 )
-  
-credentials += Credentials(Path.userHome / ".ivy2" / ".credentials") 
 
-publishTo := {
-  val repo = if (version.value endsWith "SNAPSHOT") "snapshot" else "release"
-  Some("Rhinofly Internal " + repo.capitalize + " Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-" + repo + "-local")
-}
+publishTo :=
+  Some("Rhinofly Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-release-local")
--- src/main/scala/nl/rhinofly/play/pdf/PDFUserAgent.scala
+++ src/main/scala/nl/rhinofly/play/pdf/PDFUserAgent.scala
@@ -4,32 +4,34 @@ import com.lowagie.text.Image
 import java.io.{ ByteArrayOutputStream, InputStream, OutputStream }
 import org.xhtmlrenderer.pdf.{ ITextFSImage, ITextOutputDevice, ITextUserAgent }
 import org.xhtmlrenderer.resource.{ CSSResource, ImageResource, XMLResource }
-import play.api.Play
 
 class PDFUserAgent(outputDevice: ITextOutputDevice) extends ITextUserAgent(outputDevice) {
 
   override def getImageResource(url: String): ImageResource = {
-    Play.current.resourceAsStream(url).map { stream =>
+    getResourceAsStream(url).map { stream =>
       val image = Image.getInstance(getData(stream))
       scaleToOutputResolution(image);
-      return new ImageResource(url, new ITextFSImage(image));
+      new ImageResource(url, new ITextFSImage(image));
     } getOrElse super.getImageResource(url)
   }
 
   override def getCSSResource(url: String): CSSResource =
-    Play.current.resourceAsStream(url)
+    getResourceAsStream(url)
       .map(new CSSResource(_))
       .getOrElse(super.getCSSResource(url))
 
   override def getBinaryResource(url: String): Array[Byte] =
-    Play.current.resourceAsStream(url)
+    getResourceAsStream(url)
       .map(getData(_))
       .getOrElse(super.getBinaryResource(url))
 
   override def getXMLResource(url: String): XMLResource =
-    Play.current.resourceAsStream(url)
+    getResourceAsStream(url)
       .map(XMLResource.load(_))
       .getOrElse(super.getXMLResource(url))
+
+  private def getResourceAsStream(url: String): Option[InputStream] =
+    Option(getClass.getResourceAsStream(url))
 
   private def scaleToOutputResolution(image: Image) {
     val factor = getSharedContext().getDotsPerPixel();
