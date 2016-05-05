e3ad10120615dbb2a039ac832d96bd1854623958
Nicolas Rémond <nremond+git@gmail.com>
2016-05-05 14:57:38 +0200

    Bump to Play 2.5.3

--- module/project/Build.scala
+++ module/project/Build.scala
@@ -11,9 +11,10 @@ object ApplicationBuild extends Build {
       "nu.validator.htmlparser" % "htmlparser" % "1.4"
     )
 
-    val main = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
+    val main = Project(appName, file(".")).enablePlugins(play.sbt.PlayScala).settings(
       libraryDependencies ++= appDependencies,
       version := appVersion,
+      javacOptions ++= Seq("-Xlint"),
       scalaVersion := "2.11.7",
       // hack to suppress javadoc error, see: https://play.lighthouseapp.com/projects/82401/tickets/898-javadoc-error-invalid-flag-g-when-publishing-new-module-local#ticket-898-7
       publishArtifact in(Compile, packageDoc) := false,
